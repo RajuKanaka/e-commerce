35b40b0e95a4504637d5c9d3e132d0896df21f38
Kanaka Raju <vkanaka.1213@gmail.com>
2021-12-02 19:57:28 +0530

    added comments

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,5 +1,5 @@
 import Commerce from "@chec/commerce.js";
 export const commerce = new Commerce(
-  pk_35166b87e15dd669577fa1dafa223f454624cf8b1664e,
+  "pk_35166b87e15dd669577fa1dafa223f454624cf8b1664e",
   true
 );
